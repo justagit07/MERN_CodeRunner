3c57fd8e4c95002560bf08fcf06a1c69f36283c6
justagit07 <droptelegram@outlook.com>
2024-03-05 23:46:38 +0530

    dot env is configure

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,24 @@
+import dotenv from 'dotenv'
+dotenv.config({path:"./src/.env"})
 import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
 
-const app = express()
+
+
+
+console.log(process.env.PORT)
+const app= express()
+app.use(express.json)
+app.use(cors())
+mongoose.connect()
 app.get('/', (req,res)=>
 {
-    res.send('this is working route')
+    res.send('this is you home page ')
 })
-app.listen(3000, ()=>
+app.listen(process.env.PORT ||3001, ()=>
 {
-    console.log('your server is working on the "http://localhost:3000"')
+    console.log(`your backend server is runing at "http://localhost:${process.env.PORT}"`);
     
+
 })
